778a7a0c7f5bb45a218862ecc782eca342b67b18
Madhu <s.madhu.akp@gmail.com>
2017-08-25 16:53:30 +0530

    Final

--- cricktweetDB.py
+++ cricktweetDB.py
@@ -12,7 +12,7 @@ consumer_secret = ''
 access_token = ''
 access_secret = ''
 
-
+#stemming the data
 class MyStreamListener(StreamListener):
 
 
@@ -28,7 +28,7 @@ class MyStreamListener(StreamListener):
             client = MongoClient(MONGO_CONN)
             db = client.twittercricketdb
 
-            with open('C:/Users/Madhu/Desktop/cricket.json', 'a') as f:
+            with open('C:/Users/Madhu/Desktop/cricketstreamming.json', 'a') as f:
                 f.write(data)
                 f.write('\n')
 
@@ -45,16 +45,74 @@ class MyStreamListener(StreamListener):
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
-listener = MyStreamListener(api=tweepy.API(wait_on_rate_limit=True))
-streamer = tweepy.Stream(auth=auth, listener=listener)
+#events modelling
+events=[]
+toss=[]
+toss_tags=['#elects','#elect','#bat','#bowl','#tosstime']
+toss_search=['elected to bat',' win the toss ','wins the toss','elects to bat','elected to bowl','elected to field']
+toss.append(toss_tags)
+toss.append(toss_search)
+events.append(toss)
 
-print("Tracking: " + str(WORDS))
-streamer.filter(track=WORDS)
+out=[]
+out_tags=['#wicket','#bowled','#duckout','#lbw']
+out_search=['wicket falling','another wicket down','lbw','hitwicket','catchout','walking back pavilion']
+out.append(out_tags)
+out.append(out_search)
+events.append(out)
+
+boundery=[]
+boundery_tags=['#boundery','#4runs','#four']
+boundery_search=['four runs','hits a four','4runs']
+boundery.append(boundery_tags)
+boundery.append(boundery_search)
+events.append(boundery)
+
+six=[]
+six_tags=['#sixruns','#6runs','#sixer']
+six_search=['sixer','six runs','smashed a six','6runs in 1 ball','no ball six runs','huge six','maximum six','more sixes']
+six.append(six_tags)
+six.append(six_search)
+events.append(six)
+
+scoreboard=[]
+scoreboard_tags=['#scoreboard']
+scoreboard_search=['scoreboard','need more runs to win','powerplay runs','highest runs']
+scoreboard.append(scoreboard_tags)
+scoreboard.append(scoreboard_search)
+events.append(scoreboard)
+
+amazing_moments=[]
+amazing_moments_search=['awesome catch','big six','back to back wickets','powerplay runs','century','half cenury','great vicort,'
+                        'amazing over']
+amazing_moments_tags=[]
+amazing_moments.append(amazing_moments_tags)
+amazing_moments.append(amazing_moments_search)
+events.append(amazing_moments)
+
+extra_runs=[]
+extra_runs_tags=['#wide','#noball','#4wides','#4noballs','#extraruns','#legby']
+extra_runs_search=['wide ball','no ball','extra runs','four wides','freehit']
+extra_runs.append(extra_runs_tags)
+extra_runs.append(extra_runs_search)
+events.append(extra_runs)
 
 
-# Data preprocessing
+#get data from twitter based on events
+api= tweepy.API(auth)
+for event in events:
+    for type_t_s in event:
+        for text in type_t_s:
+            for status in tweepy.Cursor(api.search, q=text).items(1):
+                # Process a single status
+                json_str = json.dumps(status._json)
+
+                with open('C:/Users/Madhu/Desktop/cricketmodelling.json', 'a') as f:
+                    f.write(json_str)
+
+
+# Twitter Data text collecting
 import codecs
-import time
 import sys
 import re
 
@@ -96,26 +154,80 @@ def parse_json_tweet(line):
 
     return [date, id, text, hashtags]
 
-file_timeordered_json_tweets = codecs.open(sys.argv[1], 'r', 'utf-8')
-fout = codecs.open(sys.argv[2], 'w', 'utf-8')
+file_model_json_tweets = codecs.open(sys.argv[1], 'r', 'utf-8')
 
-for line in file_timeordered_json_tweets:
+tlist=[]
+tweet_id_list=[]
+tweet_gmttime_list=[]
+for line in file_model_json_tweets:
     try:
 
-        [tweet_gmttime, tweet_id, text1, hashtags] = parse_json_tweet(
-            line)
+        [tweet_gmttime, tweet_id, text1, hashtags] = parse_json_tweet(line)
+        tweet_gmttime_list.append(tweet_gmttime)
+        tweet_id_list.append(tweet_id)
         t = normalize_text(text1)
-        print(t)
-        try:
-            c = time.strptime(tweet_gmttime.replace("+0000", ''), '%a %b %d %H:%M:%S %Y')
-        except:
-            print("pb with tweet_gmttime", tweet_gmttime, line)
-            pass
-        tweet_unixtime = int(time.mktime(c))
+        tlist.append(t)
 
     except:
         pass
 
 
-file_timeordered_json_tweets.close()
-fout.close()
+file_model_json_tweets.close()
+
+
+#feature extraction and clustering
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(tlist)
+
+true_k = 7
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+model.fit(X)
+
+print("Top terms per cluster:")
+order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+for i in range(true_k):
+    print("Cluster %d:" % i),
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind])
+
+
+print("\n")
+print("Cluster Prediction for streaming data")
+
+#streammingthe twitter data
+listener = MyStreamListener(api=tweepy.API(wait_on_rate_limit=True))
+streamer = tweepy.Stream(auth=auth, listener=listener)
+streamer.filter(track=WORDS)
+
+file_streaming_json_tweets = codecs.open(sys.argv[2], 'r', 'utf-8')
+
+tweetlist_stream=[]
+tweet_id_list_stream=[]
+tweet_gmttime_list_stream=[]
+for line in file_streaming_json_tweets:
+    try:
+
+        [tweet_gmttime, tweet_id, text1, hashtags] = parse_json_tweet(
+            line)
+        tweet_gmttime_list_stream.append(tweet_gmttime)
+        tweet_id_list_stream.append(tweet_id)
+        t = normalize_text(text1)
+        tweetlist_stream.append(t)
+
+    except:
+        pass
+
+
+file_streaming_json_tweets.close()
+
+
+for tweet in tweetlist_stream:
+    Y = vectorizer.transform(tweet)
+    prediction = model.predict(Y)
+    print(prediction)
+
+
